e3f857721e16802c08b2c776cdaf100c9e5f837e
tonysparks <anthony.sparks31@gmail.com>
2019-11-22 14:37:16 -0600

    fixed bit shift issues

--- src/leola/vm/Leola.java
+++ src/leola/vm/Leola.java
@@ -63,7 +63,7 @@ import leola.vm.util.ResourceLoader;
  */
 public class Leola {
 
-    public static final String VERSION = "0.10.4";
+    public static final String VERSION = "0.10.5";
     
     /**
      * Usage
